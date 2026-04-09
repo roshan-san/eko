2da0c60bca95088663438a8b22fa58e66689527d
Roshan TSX <roshantsx@email.com>
2026-04-09 10:19:51 +0530

    meow

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("hello world ")
+import pyttsx3
+
+# Initialize engine
+engine = pyttsx3.init()
+
+# Set properties (optional)
+engine.setProperty('rate', 150)   # Speed of speech
+engine.setProperty('volume', 1.0) # Volume (0.0 to 1.0)
+
+# Speak text
+engine.say("Hello Roshan, this is a text to speech test in Python.")
+
+# Run and wait
+engine.runAndWait()
